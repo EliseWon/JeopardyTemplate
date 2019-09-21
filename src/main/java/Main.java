1ab516251d2c28430c28075c1d5e17176dcb1095
dmc-account <55603701+dmc-account@users.noreply.github.com>
2019-09-20 18:18:43 -0700

    Nothing
    Elise Won

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args){
         // Put code you want to run here
         // Then press the green arrow to the left to run it
+
+        System.out.print("I don't know what I'm doing");
     }
 }
